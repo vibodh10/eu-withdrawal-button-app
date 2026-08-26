43ce6b23669fdf4b66eb10a22da4124742f1fb73
Vibodh <mvibodhk@gmail.com>
2026-08-26 16:43:12 +0100

    make inactive border visible

--- web/src/pages/DashboardPage.jsx
+++ web/src/pages/DashboardPage.jsx
@@ -236,7 +236,7 @@ export default function DashboardPage({
                 style={{
                     width: "20px",
                     height: "20px",
-                    border: "2px dashed var(--p-color-border-strong)",
+                    border: "2px dashed #8C9196",
                     borderRadius: "50%",
                     display: "inline-block",
                     boxSizing: "border-box",
